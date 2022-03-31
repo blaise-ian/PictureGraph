c98e5a5e018f32a798fed4148e6173f589096d76
blaise-ian <iblaise@uci.edu>
2022-03-31 10:19:34 -0700

    Initial Commit

---
+++ PictureGraph/PictureGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictureGraphApp.swift
+//  PictureGraph
+//
+//  Created by Ian Blaise on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct PictureGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
